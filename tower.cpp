8d04b0f9c009e017e002603d742f969c8333869a
kester <kester@lindeni.com>
2023-06-20 09:16:21 +0800

    change format of cpp file

--- tower.cpp
+++ tower.cpp
@@ -1,4 +1,5 @@
 #include "tower.h"
+
 #include <bits/stdc++.h>
 
 using namespace std;
@@ -9,13 +10,11 @@ using IdSet = set<Id>;
 const Id startId = 2;
 const MapCode startCode = "ST";
 
-struct Attr
-{
+struct Attr {
     int attack, defend, yellow, blue, red;
 };
 
-struct MonsterAttr
-{
+struct MonsterAttr {
     int hp, attack, defend, sp;
 };
 
@@ -32,13 +31,11 @@ static int initX, initY;
 
 unordered_map<Id, IdSet> adjacent;
 
-struct IdSetHash
-{
+struct IdSetHash {
     size_t operator()(const IdSet &idSet) const
     {
         size_t seed = 0;
-        for (const auto &value : idSet)
-        {
+        for (const auto &value : idSet) {
             seed ^= hash<Id>{}(value) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
         }
         return seed;
@@ -48,34 +45,23 @@ struct IdSetHash
 unordered_map<IdSet, int, IdSetHash> dp;
 unordered_set<IdSet, IdSetHash> inside;
 
-static bool isWall(const MapCode &value)
-{
-    return value == "a3";
-}
+static bool isWall(const MapCode &value) { return value == "a3"; }
 
-static bool isRoad(const MapCode &value)
-{
-    return value == "a0";
-}
+static bool isRoad(const MapCode &value) { return value == "a0"; }
 
 static void color(IdSet &ret, int flag[N][N], int i, int j)
 {
     flag[i][j] = 1;
     static const int dx[] = {0, 0, -1, 1};
     static const int dy[] = {-1, 1, 0, 0};
-    for (int k = 0; k < 4; k++)
-    {
+    for (int k = 0; k < 4; k++) {
         int ii = i + dx[k];
         int jj = j + dy[k];
-        if (ii >= 0 && ii < N && jj >= 0 && jj < N && !flag[ii][jj])
-        {
+        if (ii >= 0 && ii < N && jj >= 0 && jj < N && !flag[ii][jj]) {
             MapCode &mapCode = codeMap[ii][jj];
-            if (isRoad(mapCode))
-            {
+            if (isRoad(mapCode)) {
                 color(ret, flag, ii, jj);
-            }
-            else if (!isWall(mapCode))
-            {
+            } else if (!isWall(mapCode)) {
                 ret.insert(idMap[ii][jj]);
             }
         }
@@ -91,40 +77,28 @@ static IdSet getAdjacent(int i, int j)
     return ret;
 }
 
-static void directlyAdd(IdSet &idSet, Attr &attr, int &hp)
-{
-}
+static void directlyAdd(IdSet &idSet, Attr &attr, int &hp) {}
 
 int init(const char *body)
 {
     id2MapCode = {"roda", "wall", "ST"};
     nameMp = {
-        {"ST", "起点"},
-        {"c0", "黄钥匙"},
-        {"c1", "蓝钥匙"},
-        {"c2", "红钥匙"},
-        {"c4", "剑"},
-        {"c6", "蓝宝石"},
-        {"c7", "红宝石"},
-        {"d0", "红血瓶"},
-        {"d1", "蓝血瓶"},
-        {"h0", "黄门"},
-        {"h1", "蓝门"},
-        {"h2", "红门"},
+        {"ST", "起点"},   {"c0", "黄钥匙"}, {"c1", "蓝钥匙"}, {"c2", "红钥匙"}, {"c4", "剑"},   {"c6", "蓝宝石"},
+        {"c7", "红宝石"}, {"d0", "红血瓶"}, {"d1", "蓝血瓶"}, {"h0", "黄门"},   {"h1", "蓝门"}, {"h2", "红门"},
     };
     int buffInt;
     char buff[256];
     stringstream ss(body);
-    ss.getline(buff, 256); // title
-    ss.getline(buff, 256); // $$
-    ss >> initHp >> initAttr.attack >> initAttr.defend >> buffInt >> buffInt >> initAttr.yellow >> initAttr.blue >> initAttr.red >> buffInt >> initX >> initY;
-    // cout << initHp << ' ' << initAttr.attack << ' ' << initAttr.defend << ' ' << initAttr.yellow << ' ' << initAttr.blue << ' ' << initAttr.red << endl;
-    ss.getline(buff, 256); // \n
-    ss.getline(buff, 256); // $$
-    for (int i = 0; i < N; i++)
-    {
-        for (int j = 0; j < N; j++)
-        {
+    ss.getline(buff, 256);  // title
+    ss.getline(buff, 256);  // $$
+    ss >> initHp >> initAttr.attack >> initAttr.defend >> buffInt >> buffInt >> initAttr.yellow >> initAttr.blue >>
+        initAttr.red >> buffInt >> initX >> initY;
+    // cout << initHp << ' ' << initAttr.attack << ' ' << initAttr.defend << ' ' << initAttr.yellow << ' ' <<
+    // initAttr.blue << ' ' << initAttr.red << endl;
+    ss.getline(buff, 256);  // \n
+    ss.getline(buff, 256);  // $$
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
             ss >> codeMap[i][j];
         }
     }
@@ -137,57 +111,47 @@ int init(const char *body)
     //     cout << endl;
     // }
 
-    ss.getline(buff, 256); // \n
-    ss.getline(buff, 256); // #
-    ss.getline(buff, 256); // $$
-    while (strncmp(buff, "#", 1) != 0)
-    {
+    ss.getline(buff, 256);  // \n
+    ss.getline(buff, 256);  // #
+    ss.getline(buff, 256);  // $$
+    while (strncmp(buff, "#", 1) != 0) {
         ss.getline(buff, 256);
-        if (strncmp(buff, "mapcode=", 8) == 0)
-        {
+        if (strncmp(buff, "mapcode=", 8) == 0) {
             string mapCode = buff + 8;
-            while (*mapCode.rbegin() == '\r')
-                mapCode.pop_back();
+            while (*mapCode.rbegin() == '\r') mapCode.pop_back();
             MonsterAttr attr;
-            ss.getline(buff, 256); // prefix=
-            ss.getline(buff, 256); // name=
+            ss.getline(buff, 256);  // prefix=
+            ss.getline(buff, 256);  // name=
             string name = buff + 5;
-            while (*name.rbegin() == '\r')
-                name.pop_back();
-            ss.getline(buff, 256); // bfEvents=
-            ss.getline(buff, 256); // afEvents=
-            ss.getline(buff, 256); // hp=
+            while (*name.rbegin() == '\r') name.pop_back();
+            ss.getline(buff, 256);  // bfEvents=
+            ss.getline(buff, 256);  // afEvents=
+            ss.getline(buff, 256);  // hp=
             attr.hp = atoi(buff + 3);
-            ss.getline(buff, 256); // attack=
+            ss.getline(buff, 256);  // attack=
             attr.attack = atoi(buff + 7);
-            ss.getline(buff, 256); // defend=
+            ss.getline(buff, 256);  // defend=
             attr.defend = atoi(buff + 7);
-            ss.getline(buff, 256); // coins=
-            ss.getline(buff, 256); // exp=
-            ss.getline(buff, 256); // sp=
+            ss.getline(buff, 256);  // coins=
+            ss.getline(buff, 256);  // exp=
+            ss.getline(buff, 256);  // sp=
             attr.sp = atoi(buff + 3);
-            // cout << mapCode << ' ' << attr.name << ' ' << attr.hp << ' ' << attr.attack << ' ' << attr.defend << ' ' << attr.sp << endl;
+            // cout << mapCode << ' ' << attr.name << ' ' << attr.hp << ' ' << attr.attack << ' ' << attr.defend << ' '
+            // << attr.sp << endl;
             nameMp[mapCode] = move(name);
             monsterMp.emplace(move(mapCode), move(attr));
         }
     }
 
     int top = 3;
-    for (int i = 0; i < N; i++)
-    {
-        for (int j = 0; j < N; j++)
-        {
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
             MapCode &code = codeMap[i][j];
-            if (isRoad(code))
-            {
+            if (isRoad(code)) {
                 idMap[i][j] = 0;
-            }
-            else if (isWall(code))
-            {
+            } else if (isWall(code)) {
                 idMap[i][j] = 1;
-            }
-            else
-            {
+            } else {
                 idMap[i][j] = top++;
                 id2MapCode.push_back(codeMap[i][j]);
             }
@@ -204,14 +168,11 @@ int init(const char *body)
     //     cout << endl;
     // }
 
-    for (int i = 0; i < N; i++)
-    {
-        for (int j = 0; j < N; j++)
-        {
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
             MapCode &mapCode = codeMap[i][j];
             Id &id = idMap[i][j];
-            if (!isWall(mapCode) && !isRoad(mapCode))
-            {
+            if (!isWall(mapCode) && !isRoad(mapCode)) {
                 adjacent[id] = getAdjacent(i, j);
                 // cout << nameMp[mapCode] << ":";
                 // for (const auto &i : adjacent[id])
--- tower.h
+++ tower.h
@@ -1,9 +1,8 @@
 #pragma once
 
-extern "C"
-{
-    int init(const char* body);
+extern "C" {
+int init(const char* body);
 
-    // If NULL is passed in, it is directly output in std::out, otherwise it is output in buffer.
-    int spfa(char* buffer = 0);
+// If NULL is passed in, it is directly output in std::out, otherwise it is output in buffer.
+int spfa(char* buffer = 0);
 }
